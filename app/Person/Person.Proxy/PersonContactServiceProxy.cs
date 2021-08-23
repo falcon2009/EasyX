33e4cfc06f95d753ef2b15e34aa08b34ab88fa10
falcon1969 <falcon.audia6@gmail.com>
2021-08-23 23:19:09 +0300

    add person proxy

---
+++ app/Person/Person.Proxy/PersonContactServiceProxy.cs
@@ -0,0 +1,14 @@
+﻿using EasyX.Http;
+using EasyX.Infra.Api;
+using Person.Share.Key;
+using ServiceCore.Services;
+using System.Net.Http;
+
+namespace Person.Proxy
+{
+    public class PersonContactServiceProxy : ModelServiceHttpGeneric<PersonContactKey>
+    {
+        public PersonContactServiceProxy(IHttpService httpService, HttpClient httpClient, ITypeResolver typeResolver, string controller) : base(httpService, httpClient, typeResolver, controller)
+        { }
+    }
+}
---
+++ app/Person/Person.Proxy/PersonServiceProxy.cs
@@ -0,0 +1,14 @@
+﻿using EasyX.Http;
+using EasyX.Infra.Api;
+using Person.Share.Key;
+using ServiceCore.Services;
+using System.Net.Http;
+
+namespace Person.Proxy
+{
+    public class PersonServiceProxy : ModelServiceHttpGeneric<PersonKey>
+    {
+        public PersonServiceProxy(IHttpService httpService, HttpClient httpClient, ITypeResolver typeResolver, string controller) : base(httpService, httpClient, typeResolver, controller)
+        { }
+    }
+}
